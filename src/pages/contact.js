9f9b7792259c744c430db42e03b2e09bfeabf845
Jack Gugi <47448452+jackgugi@users.noreply.github.com>
2019-12-27 11:15:20 +0000

    Contact Form Change

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,17 +8,25 @@ export default () => (
     <p>07975 658 423</p>
     <p>jackgugi5213@hotmail.co.uk</p>
 
-    <form name="contact" netlify>
-      <h2>Send us a message</h2>
-	  <p>
-	    <label>Name <input type="text" name="name" /></label>
-	  </p>
-	  <p>
-	    <label>Email <input type="email" name="email" /></label>
-	  </p>
-	  <p>
-	    <button type="submit">Send</button>
-	  </p>
+    <form id="contact-form" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+	  <input type="hidden" name="bot-field" />
+	  <input type="hidden" name="form-name" value="contact" />
+	  <div className="field half first">
+	    <label htmlFor="name">Name</label>
+	    <input type="text" name="name" id="name" />
+	  </div>
+	  <div className="field half">
+	    <label htmlFor="email">Email</label>
+	    <input type="text" name="email" id="email" />
+	  </div>
+	  <div className="field">
+	    <label htmlFor="message">Message</label>
+	    <textarea name="message" id="message" rows="6" />
+	  </div>
+	  <div className="actions">
+	      <input type="submit" value="Send Message" className="special" />
+	      <input type="reset" value="Clear" />
+	  </div>
 	</form>
   </Layout>
 )
