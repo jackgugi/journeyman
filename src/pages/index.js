db98b247b16aa44d71e249c73c4de301fcaa933b
Jack Gugi <47448452+jackgugi@users.noreply.github.com>
2023-04-27 20:20:19 +0100

    Delete src directory

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/layout"
-
-export default () => (
-  <Layout>
-    <h1>We Code With Soul</h1>
-    <p>No restricting themes or templates!</p>
-  </Layout>
-)
